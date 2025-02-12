ce010911bc13e0aedd6c752b4b47b9f376f2dda0
Damont05 <luisdanielmonterofalcon@gmail.com>
2025-02-12 17:39:01 -0300

    Fix-BACK-004: Modificar respuestas de servicios Auth

---
+++ backend/src/utils/constants.js
@@ -0,0 +1,11 @@
+
+export class Constants {
+    static EMAIL_FORMAT = 'Formato de Email Inválido';
+    static MISSING_FIELDS = 'Todos los campos son requeridos';
+    static USER_EXIST = 'Usuario ya se encuentra registrado';
+    static USER_REGISTER_SUCCESS = 'Usuario registrado con éxito';
+    static USER_NOT_FOUND = 'Usuario no existe';
+    static USER_LOGIN_SUCCESS = 'Login con éxito';
+    static PASSWORD_INVALID = 'Contraseña inválida';
+    static INTERNAL_SERVER_ERROR = 'El sitio web está experimentando problemas técnicos. Inténtelo nuevamente más tarde';
+}
